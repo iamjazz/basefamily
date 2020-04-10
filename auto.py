48907970af482afdc5b1925186886f79d136995e
iamjazz <52528437+iamjazz@users.noreply.github.com>
2020-04-11 01:44:18 +0800

    Update auto.py

--- auto.py
+++ auto.py
@@ -20,7 +20,7 @@ if autodecode.isdigit():
             print ("Base16解码后字符为:",check16)
             s62=autodecode
             d62=base62.decode(s62)
-            print("Base61解码后字符为:", d62)
+            print("Base62解码后字符为:", d62)
         except:
             s16 = str(autodecode)
             d16 = base64.b16decode(s16)
@@ -28,36 +28,111 @@ if autodecode.isdigit():
             print ("Base16解码后字符为:",check16)
             s62=autodecode
             d62=base62.decode(s62)
-            print("Base61解码后字符为:", d62)
+            print("Base62解码后字符为:", d62)
     except:
         s62=autodecode
         d62=base62.decode(s62)
-        print("Base61解码后字符为:", d62)
+        print("Base62解码后字符为:", d62)
 else:    
     try:
         try:
             try:
                 try:
                     try:
-                        s64 = str(autodecode)
-                        d64 = base64.b64decode(s64)
-                        check64 = str(d64, 'utf8')
-                        print ("Base64解码后字符为:",check64)
+                        try:
+                            try:
+                                try:
+                                    try:
+                                        try:
+                                            try:
+                                                try:
+                                                    try:
+                                                        try:
+                                                            try:
+                                                                try:
+                                                                    try:
+                                                                        s64 = str(autodecode)
+                                                                        d64 = base64.b64decode(s64)
+                                                                        check64 = str(d64, 'utf8')
+                                                                        print ("Base64解码后字符为:",check64)
+                                                                    except:
+                                                                        s85 = str(autodecode)
+                                                                        d85 = base64.b85decode(s85)
+                                                                        check85 = str(d85, 'utf8')
+                                                                        print ("Base85解码后字符为:",check85)
+                                                                        s62=autodecode
+                                                                        d62=base62.decode(s62)
+                                                                        print("Base61解码后字符为:", d62)
+                                                                except:    
+                                                                    s58 = str(autodecode)
+                                                                    d58 = base58.b58decode(s58)
+                                                                    check58 = str(d58, 'utf8')
+                                                                    print ("Base58解码后字符为:",check58)
+                                                            except:
+                                                                s58 = str(autodecode)
+                                                                d58 = base58.b58decode(s58)
+                                                                check58 = str(d58, 'utf8')
+                                                                print ("Base58解码后字符为:",check58)
+                                                        except:
+                                                            s91=autodecode
+                                                            d91=base91.decode(s91)
+                                                            check91 = str(d91, 'utf8')
+                                                            print("Base91解码后字符为:",check91)
+                                                    except:
+                                                        s32 = str(autodecode)
+                                                        d32 = base64.b32decode(s32)
+                                                        check32 = str(d32, 'utf8')
+                                                        print ("Base32解码后字符为:",check32)
+                                                except:
+                                                    s85 = str(autodecode)
+                                                    d85 = base64.b85decode(s85)
+                                                    check85 = str(d85, 'utf8')
+                                                    print ("Base85解码后字符为:",check85)
+                                            except:
+                                                s58 = str(autodecode)
+                                                d58 = base58.b58decode(s58)
+                                                check58 = str(d58, 'utf8')
+                                                print ("Base58解码后字符为:",check58)
+                                        except:
+                                            s58 = str(autodecode)
+                                            d58 = base58.b58decode(s58)
+                                            check58 = str(d58, 'utf8')
+                                            print ("Base58解码后字符为:",check58)
+                                    except:
+                                        s91=autodecode
+                                        d91=base91.decode(s91)
+                                        check91 = str(d91, 'utf8')
+                                        print("Base91解码后字符为:",check91)
+                                except:
+                                    s32 = str(autodecode)
+                                    d32 = base64.b32decode(s32)
+                                    check32 = str(d32, 'utf8')
+                                    print ("Base32解码后字符为:",check32)
+                            except:
+                                s58 = str(autodecode)
+                                d58 = base58.b58decode(s58)
+                                check58 = str(d58, 'utf8')
+                                print ("Base58解码后字符为:",check58)
+                        except:
+                            s91=autodecode
+                            d91=base91.decode(s91)
+                            check91 = str(d91, 'utf8')
+                            print("Base91解码后字符为:",check91)
                     except:
-                        s85 = str(autodecode)
-                        d85 = base64.b85decode(s85)
-                        check85 = str(d85, 'utf8')
-                        print ("Base85解码后字符为:",check85)
+                        s32 = str(autodecode)
+                        d32 = base64.b32decode(s32)
+                        check32 = str(d32, 'utf8')
+                        print ("Base32解码后字符为:",check32)
                 except:
-                    s58 = str(autodecode)
-                    d58 = base58.b58decode(s58)
-                    check58 = str(d58, 'utf8')
-                    print ("Base58解码后字符为:",check58)
+                    s91=autodecode
+                    d91=base91.decode(s91)
+                    check91 = str(d91, 'utf8')
+                    print("Base91解码后字符为:",check91)
             except:
-                s91=autodecode
-                d91=base91.decode(s91)
-                check91 = str(d91, 'utf8')
-                print("Base91解码后字符为:",check91)
+                s32 = str(autodecode)
+                d32 = base64.b32decode(s32)
+                check32 = str(d32, 'utf8')
+                print ("Base32解码后字符为:",check32)
         except:
             s32 = str(autodecode)
             d32 = base64.b32decode(s32)
@@ -66,7 +141,4 @@ else:
     except:
         s62=autodecode
         d62=base62.decode(s62)
-        print("Base61解码后字符为:", d62)
-    else:
-        print("OK")
-
+        print("Base62解码后字符为:", d62)
